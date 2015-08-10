161b4a4130ab1af006c31d25333013ff76c3f336
CoyoteBringsFire <aumkara@gmail.com>
2015-08-09 20:47:10 -0700

    finished unimplemented tests

--- test/mix_logs.js
+++ test/mix_logs.js
@@ -5,21 +5,30 @@ var should	= require('should'),
 	async 	= require('async'),
 	util 	= require('util');
 
+function verifyMerge(lines) {
+	var lastTime 	= lines[lines.length-1].time;
+	for (var i = lines.length - 2; i >= 0; i--) {
+		lines[i].time.should.not.be.greaterThan( lastTime );
+	};
+}
+
 describe("mix_logs", function suite() {
-	var NUMBER_ENTRIES 		= 10,
-		NUMBER_OF_LOGFILES 	= 100;
+	var NUMBER_OF_ENTRIES 	= 10,
+		NUMBER_OF_LOGFILES 	= 100,
+		logs 				= [];
 
 	this.timeout(10000);
 
 	before(function removeTmpDir(done) {
 		rimraf("./tmp/", done);
 	});
+	
 	before(function makeTmpDir(done) {
 		fs.mkdir("./tmp/", done);
 	});
+	
 	before(function setupTestData(done) {
-		var logs 	= [],
-			logStream, i;
+		var logStream, i;
 
 		for( i = 0; i < NUMBER_OF_LOGFILES; i++ ) {
 			logStream = fs.createWriteStream( util.format("./tmp/test%d",i) );
@@ -28,18 +37,23 @@ describe("mix_logs", function suite() {
 				stream: logStream,
 	    		level: "info"
 	    	});
-	    	logs.push(log);
+	    	logs.push({ 
+	    		filename: util.format("./tmp/test%d", i),
+	    		stream: log
+	    	});
 		}
+
 		async.each(logs, function writeLogs(log, doneCreatingLogs) {
 			var j;
-			async.times( NUMBER_ENTRIES, function logTestData(count, doneLogging) {
+			async.timesSeries( NUMBER_OF_ENTRIES, function logTestData(count, doneLogging) {
 				setTimeout(function onTimeout() {
-					log.info("test");
+					log.stream.info("test");
 					doneLogging();
 				}, Math.floor( Math.random() * 100 ) );
 			}, doneCreatingLogs);
 		}, done);
 	});
+	
 	it("should throw an error if constructed with a string", function doTest(done) {
 		var mixLogs=require('../lib/mix_logs');
 		(function() {
@@ -47,6 +61,7 @@ describe("mix_logs", function suite() {
 		}).should.throw();
 		done();
 	});
+	
 	it("should throw an error if constructed without an argument", function doTest(done) {
 		var mixLogs=require('../lib/mix_logs');
 		(function() {
@@ -54,13 +69,34 @@ describe("mix_logs", function suite() {
 		}).should.throw();
 		done();
 	});
-	it("should return a readable stream of the merged files - 1 files", function doTest(done) {
-		should.fail("test incomplete");
+	
+	it("should return an array of the merged files - 1 file", function doTest(done) {
+		var mixLogs=require('../lib/mix_logs'),
+			mixedLogs;
+		mixedLogs = mixLogs(logs.slice(0,1));
+		mixedLogs.should.be.an.instanceOf(Array);
+		mixedLogs.length.should.equal(NUMBER_OF_ENTRIES*1);
+		verifyMerge( mixedLogs );
+		done();
 	});
-	it("should return a readable stream of the merged files - 10 file", function doTest(done) {
-		should.fail("test incomplete");
+	
+	it("should return an array of the merged files - 10 files", function doTest(done) {
+		var mixLogs=require('../lib/mix_logs'),
+			mixedLogs;
+		mixedLogs = mixLogs(logs.slice(0,10));
+		mixedLogs.should.be.an.instanceOf(Array);
+		mixedLogs.length.should.equal(NUMBER_OF_ENTRIES*10);
+		verifyMerge( mixedLogs );
+		done();
 	});
-	it("should return a readable stream of the merged files - 100 files", function doTest(done) {
-		should.fail("test incomplete");
+	
+	it("should return an array of the merged files - 100 files", function doTest(done) {
+		var mixLogs=require('../lib/mix_logs'),
+			mixedLogs;
+		mixedLogs = mixLogs(logs.slice(0,100));
+		mixedLogs.should.be.an.instanceOf(Array);
+		mixedLogs.length.should.equal(NUMBER_OF_ENTRIES*100);
+		verifyMerge( mixedLogs );
+		done();
 	});
 });
